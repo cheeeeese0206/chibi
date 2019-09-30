7bdb2d1d9533c29e9328e665faf79a66b742d54c
SHINODA Saki <21716045@jwu.ac.jp>
2019-09-30 16:52:43 +0900

    my first commit

--- hello.py
+++ hello.py
@@ -2,9 +2,9 @@
 def calc(s):
     print('s=',s)
     nums = map(int,s.split('+'))
+    nums = map(int,s.split('*'))
     print('nums=',nums)
     return sum(nums)
 
-print(calc("1"))
-print(calc("1+2"))
-print(calc("1+2+3"))
+print(calc("1*2+3"))
+print(calc("1+2*3"))
