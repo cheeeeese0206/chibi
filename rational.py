62599189dc7765ea53caef128de898fd098abc4a
SHINODA Saki <21716045@jwu.ac.jp>
2019-10-07 16:29:53 +0900

    5/6

--- rational.py
+++ rational.py
@@ -1,7 +1,20 @@
 
 class Q(object):
-    def __init__(self,a,b):
+    def __init__(self,a,b=1):
         self.a = a
         self.b = b
-q = Q(1,2)
-print(q)
+    def __repr__(self):
+        if self.b == 1:
+            return str(self.a)
+        return f'{self.a}/{self.b}'
+    def __add__(self, q):
+        a = self.a
+        b = self.b
+        c = q.a
+        d = q.b
+        return Q(a*d+b*c,b*d)
+
+
+q1 = Q(1,2)
+q2 = Q(1,3)
+print(q1 + q2)
