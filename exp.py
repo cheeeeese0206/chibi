3034d422aa5fbe3d32a7d30a0086c3e8c960a8dd
SHINODA Saki <21716045@jwu.ac.jp>
2019-10-14 16:10:15 +0900

    Add

--- exp.py
+++ exp.py
@@ -4,7 +4,7 @@ class Val(object):
     def __init__(self, value = 0):
         self.value = value
     def __repr__(self):
-            return f'VAl({self.value})'
+        return f'Val({self.value})'
     def eval(self):
         return self.value
 
@@ -12,4 +12,22 @@ v = Val(1)
 print(v)
 assert v.eval() == 1
 
+class Add(object):
+    __slots__ = ['left', 'right']
+    def __init__(self, a, b):
+        self.left  = a
+        self.right = b
+    def eval(self):
+        return self.left.eval() + self.right.eval()
 
+e = Add(Val(1),Val(2))
+assert e.eval() == 3
+
+e = Add(Val(1),Add(Val(2),Val(3))
+assert e.eval() == 6
+
+
+
+
+print()
+print()
