d1faa97c5f2e87445ab47a119662652ded8a74c5
SHINODA Saki <21716045@jwu.ac.jp>
2019-10-28 16:36:02 +0900

    output Add,Mul

--- parser.py
+++ parser.py
@@ -1,10 +1,10 @@
-from exp import Val, Add
-
+from exp import Val, Add, Mul
+'''
 def parse(s: str):
     num = int(s)
     return Val(num)
 
-'''
+
 e = parse("123")
 print(e)
 
@@ -15,7 +15,7 @@ print('pos', pos)
 s1 = s[0:pos]
 s2 = s[pos+1:]
 print(s,s1,s2) #+記号で分割
-'''
+
 
 def parse(s: str):
     pos = s.find('+')
@@ -26,6 +26,20 @@ def parse(s: str):
         s1 = s[0:pos]
         s2 = s[pos+1:]
         return Add(parse(s1), parse(s2))
+        '''
 
-e = parse("1+2+3")
+def parse(s: str):
+    if s.find('+') > 0:
+        pos = s.find('+')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Add(parse(s1), parse(s2))
+    if s.find('*') > 0:
+        pos = s.find('*')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Mul(parse(s1), parse(s2))
+    return Val(int(s))
+
+e = parse("1+2*3")
 print(e,e.eval())
