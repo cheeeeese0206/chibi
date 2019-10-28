4b8154306fdd305f895a29d0461361525749e55d
SHINODA Saki <21716045@jwu.ac.jp>
2019-10-28 16:50:41 +0900

    output Expr

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-from exp import Val, Add, Mul
+from exp import Val, Add, Mul, Sub, Div
 '''
 def parse(s: str):
     num = int(s)
@@ -26,7 +26,7 @@ def parse(s: str):
         s1 = s[0:pos]
         s2 = s[pos+1:]
         return Add(parse(s1), parse(s2))
-        '''
+'''
 
 def parse(s: str):
     if s.find('+') > 0:
@@ -34,12 +34,23 @@ def parse(s: str):
         s1 = s[0:pos]
         s2 = s[pos+1:]
         return Add(parse(s1), parse(s2))
+    if s.find('-') > 0:
+        pos = s.find('-')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Sub(parse(s1), parse(s2))
     if s.find('*') > 0:
         pos = s.find('*')
         s1 = s[0:pos]
         s2 = s[pos+1:]
         return Mul(parse(s1), parse(s2))
+    if s.find('/') > 0:
+        pos = s.find('/')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Div(parse(s1), parse(s2))
     return Val(int(s))
-
-e = parse("1+2*3")
-print(e,e.eval())
+    
+e = parse("1-2-3")
+print(e, e.eval())
+assert e.eval() == -4
