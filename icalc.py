cad5b3ef7cbf8c887627cce09975da027f22d55d
SHINODA Saki <21716045@jwu.ac.jp>
2019-11-25 16:23:04 +0900

    input

--- icalc.py
+++ icalc.py
@@ -20,6 +20,22 @@ def calc(t):
     print(f'TODO {t.tag}')
     return 0
 
-t = parser('1+2*3+4*5')
-print(repr(t))
-print(calc(t))
+#t = parser('1+2*3+4*5')
+#print(repr(t))
+#print(calc(t))
+def run(s:str):
+    print('TODO', s)
+
+def main():
+    s = input('>>> ')
+    run(s)
+
+
+def main():
+   s = input('$ ')
+   t = parser(s)
+   print(calc(t))
+
+if __name__ == '__main__':
+    main()
+
